5ebf2646f92f6715f3816b2094cde4c1d9d20d09
gosho-kazuya <ketsume0211@gmail.com>
2016-09-12 23:48:35 +0900

    add web for install mysqli

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php phpinfo();
+<?php
+$mysqli = new mysqli('db', 'root', 'root', 'app');
+
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error);
+}
+
+echo 'Success... ' . $mysqli->host_info . "\n";
+
+$mysqli->close();
